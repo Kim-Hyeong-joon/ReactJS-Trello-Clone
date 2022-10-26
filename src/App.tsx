b7b19a93ba47b405b83a4cd885228b7e007d3e82
Hyeong-Joon <gudwns1020@naver.com>
2022-10-27 00:52:10 +0900

    #7.10 Cross Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,20 @@ function App() {
         };
       });
     }
+    if (destination?.droppableId !== source.droppableId) {
+      if (!destination) return;
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination?.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination?.droppableId]: destinationBoard,
+        };
+      });
+    }
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
